0c76d604954e623ce61cbb9e4b84512e10927d65
QMinjia <maidoudou@sjtu.edu.cn>
2020-11-13 13:03:44 +0800

    Update video.js

--- js/video.js
+++ js/video.js
@@ -6,9 +6,60 @@ window.addEventListener("load", function() {
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
+	video.play();
+	document.getElementById("volume").innerHTML=video.volume *100 +"%";
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	
+	console.log("Pause Video");
+	video.pause();
 });
 
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate*0.9;
+  	console.log("Speed: " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate*1.1;
+  	console.log("Speed: " + video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	let vid = document.getElementById("myVideo");
+	if (vid.currentTime <= vid.duration-5){
+		vid.currentTime = vid.currentTime + 5;
+	}else{
+	 	vid.currentTime = 0;
+	}
+  	console.log("Current location: " + vid.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+    if (video.muted){
+    	video.muted = 0;
+    	document.getElementById('mute').innerHTML="Mute";
+    	console.log("Unmuted");
+    }else{
+    	video.muted = 1;
+    	document.getElementById('mute').innerHTML="Unmute";
+    	console.log("Muted");
+    }
+});
+
+document.querySelector("#volumeSlider").addEventListener("change", function() {
+	let volume = document.querySelector("#volumeSlider").value/100;
+	video.volume = volume;
+	document.getElementById("volume").innerHTML = volume*100 + "%";
+	console.log("Volume: " + volume);
+});
+
+document.querySelector("#old").addEventListener("click", function() {
+	document.getElementById("myVideo").style.filter = "grayscale(100%)";
+	console.log("Old School");
+});
+
+document.querySelector("#original").addEventListener("click", function() {
+	document.getElementById("myVideo").style.filter = "grayscale(0%)";
+	console.log("original");
+});
